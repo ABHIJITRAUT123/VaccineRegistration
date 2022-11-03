b21dce8e2cf513010843b8d595098d8f1424c2fa
Abhijit Raut <abhijitraut150@gmail.com>
2022-11-03 10:36:37 +0000

    Update index.js and yarn.lock

--- src/index.js
+++ src/index.js
@@ -3,7 +3,11 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("hello");
+  res.send(`<h2>Vaccine Registration Home Page</h2>
+            <button>Sign Up</button>
+            <span>
+              <button>Log In</button>
+            </span>`);
 });
 
 app.listen(8080);
